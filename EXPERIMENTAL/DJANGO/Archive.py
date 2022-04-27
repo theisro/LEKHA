54eeadc027941a0c41feb45a67ac49b57ed459da
ya5ha5 <yashas@cema.in>
2022-04-27 21:13:17 +0530

    Create Archive.py

---
+++ EXPERIMENTAL/DJANGO/Archive.py
@@ -0,0 +1,30 @@
+# This is a first prototype for an Archive Class.
+# Please add to this. What fields should it have?
+
+
+
+from django.db import models
+   from django.utils import timezone
+   from django.contrib.auth.models import User
+   class Archive(models.Model):
+       STATUS_CHOICES = (
+           ('draft', 'Draft'),
+           ('published', 'Published'),
+       )
+       title = models.CharField(max_length=250)
+       slug = models.SlugField(max_length=250,
+                               unique_for_date='publish')
+       author = models.ForeignKey(User,
+                                 on_delete=models.CASCADE,
+                                 related_name='blog_posts')
+       ArchiveDescription = models.TextField()
+       publish = models.DateTimeField(default=timezone.now)
+       created = models.DateTimeField(auto_now_add=True)
+       updated = models.DateTimeField(auto_now=True)
+       status = models.CharField(max_length=10,
+                                 choices=STATUS_CHOICES,
+                                 default='draft')
+       class Meta:
+           ordering = ('-publish',)
+       def __str__(self):
+           return self.title
