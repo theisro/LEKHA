21cdf39db8835fb5343383de5126e60491a3b1f3
ya5ha5 <yashas@cema.in>
2022-05-03 12:13:27 +0530

    Update File.py

--- EXPERIMENTAL/DJANGO/File.py
+++ EXPERIMENTAL/DJANGO/File.py
@@ -13,3 +13,23 @@
 # Pipe and named pipe (also called FIFO)
 # Socket”
 
+
+# FILES are associated with INODES
+
+Number of hard links associated with the file
+File length in bytes
+Device ID (i.e., an identifier of the device containing the file)
+Inode number that identifies the file within the filesystem
+UID of the file owner
+User group ID of the file
+Several timestamps that specify the inode status change time, the last access time, and the last modify time
+Access rights and file mode.
+
+
+*** Access Rights and File Mode ***
+The potential users of a file fall into three classes:
+
+1. The user who is the owner of the file
+2. The users who belong to the same group as the file, not including the owner
+3. All remaining users (others)”
+
