199d1c40fdb3b764bd36c27606816532ad0d0acd
ya5ha5 <yashas@cema.in>
2022-05-03 12:25:49 +0530

    Create Archiving.py

---
+++ EXPERIMENTAL/DJANGO/Archiving.py
@@ -0,0 +1,15 @@
+# Things to implement in Archiving.
+
+1. An archiving process descriptor that includes information about the current state of the process.
+2. A process ID
+
+# Definition of a process in LINUX 
+#    a process is an instance of a program in execution. You might think of it as the collection of data structures that fully
+#    describes how far the execution of the program has progressed.”
+#    They contain process descriptor — a task_struct type structure whose fields contain all the information related to a single process.”
+#    They contain a "PROCESS State" field.
+#  
+
+
+
+
