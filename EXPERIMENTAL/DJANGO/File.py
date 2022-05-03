bd2ffcbf023049035eee02e340367ecdeb4daaec
ya5ha5 <yashas@cema.in>
2022-05-03 12:08:52 +0530

    Create File.py

---
+++ EXPERIMENTAL/DJANGO/File.py
@@ -0,0 +1,15 @@
+# FOLDER.py should perhaps be replaced with FILE.py
+
+# A Directory node(or a Folder node) will contain information about Files and Directores(FOLDERS) beneath it.
+# The root directory by convention is called "/"
+# Each "process of archiving" is associated with a current working directory.
+
+# A file is of the following types:
+  # “Regular file
+# Directory
+# Symbolic link
+# Block-oriented device file
+# Character-oriented device file
+# Pipe and named pipe (also called FIFO)
+# Socket”
+
