1e5798a7fe7e18d3f968644037fcf7097e085d0a
ya5ha5 <yashas@cema.in>
2022-05-03 11:51:52 +0530

    Update User.py

--- EXPERIMENTAL/DJANGO/User.py
+++ EXPERIMENTAL/DJANGO/User.py
@@ -3,5 +3,7 @@
 # The USER is given access to ARCHIVES.
 # Some USERs can access multiple ARCHIVES.
 
+#  ******** FEATURES ***********
+# “An authentication mechanism for verifying the user's identity”
 
 
