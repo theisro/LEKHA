89d33990f42bee830c463070389bc430ec509b12
Arko <bhattacharjee.arkoprabho@gmail.com>
2022-06-03 11:32:37 +0530

    Landing page now available at /

--- EXPERIMENTAL/djangoactual/djangoactual/urls.py
+++ EXPERIMENTAL/djangoactual/djangoactual/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('landingpage/', include('landingpage.urls')),
+    path('', include('landingpage.urls')),
     path('admin/', admin.site.urls),
 ]
