59962b400d2c99851ec9d2d20885e08760096788
Arko <bhattacharjee.arkoprabho@gmail.com>
2022-06-10 23:24:26 +0530

    Added dummy pages for About Us, Features, Contact, Partners, and Report a Bug. Wired up their click functionality as well.

--- EXPERIMENTAL/djangoactual/landingpage/urls.py
+++ EXPERIMENTAL/djangoactual/landingpage/urls.py
@@ -4,4 +4,9 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('features/', views.features, name='features'),
+    path('contact/', views.contact, name='contact'),
+    path('about/', views.about, name='about'),
+    path('partners/', views.partners, name='partners'),
+    path('report/', views.report, name='report'),
 ]
--- EXPERIMENTAL/djangoactual/landingpage/views.py
+++ EXPERIMENTAL/djangoactual/landingpage/views.py
@@ -3,3 +3,23 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'index.html')
+
+
+def features(request):
+    return render(request, 'features.html')
+
+
+def contact(request):
+    return render(request, 'contact.html')
+
+
+def about(request):
+    return render(request, 'about.html')
+
+
+def partners(request):
+    return render(request, 'partners.html')
+
+
+def report(request):
+    return render(request, 'report.html')
